5c3a09777c0839e83483170308fff28ba0e7b615
taea <igiari@gmail.com>
2013-12-02 06:20:39 +0900

    Add UITableView

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -6,15 +6,19 @@ class AppDelegate
 
     controller = TapController.alloc.initWithNibName(nil, bundle: nil)
     nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
-    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
-    tab_controller.viewControllers = [nav_controller]
-    @window.rootViewController = tab_controller
 
     other_controller = UIViewController.alloc.initWithNibName(nil, bundle: nil)
     other_controller.title = "Other"
     other_controller.view.backgroundColor = UIColor.purpleColor
     tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
     tab_controller.viewControllers = [nav_controller, other_controller]
+    @window.rootViewController = tab_controller
+
+    controller = TapController.alloc.initWithNibName(nil, bundle: nil)
+    nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
+    alphabet_controller = AlphabetController.alloc.initWithNibName(nil, bundle: nil)
+    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
+    tab_controller.viewControllers = [alphabet_controller, nav_controller]
     @window.rootViewController = tab_controller
     true
   end
