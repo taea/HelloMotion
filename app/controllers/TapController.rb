940b86aeef1916b1b3166613eebbff33ea7e2f50
taea <igiari@gmail.com>
2013-12-02 04:55:50 +0900

    Add UIBarButtonItem

--- app/controllers/TapController.rb
+++ app/controllers/TapController.rb
@@ -10,6 +10,13 @@ class TapController < UIViewController
     @label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 2)
     
     self.view.addSubview @label
-    self.title = "Tap"
+    self.title = "Tap (#{self.navigationController.viewControllers.count})"
+    right_button = UIBarButtonItem.alloc.initWithTitle("Push", style: UIBarButtonItemStyleBordered, target: self, action: 'push')
+    self.navigationItem.rightBarButtonItem = right_button
+  end
+
+  def push
+    new_controller = TapController.alloc.initWithNibName(nil, bundle: nil)
+    self.navigationController.pushViewController(new_controller, animated: true)
   end
 end
