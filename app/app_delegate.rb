86a5bbff26287b96478d96e5c643b73025c5e6f5
taea <igiari@gmail.com>
2013-12-02 02:32:01 +0900

    Views

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,11 +1,20 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    alert = UIAlertView.new
-    alert.message = "Hello Motion!"
-    alert.show
+    # UIScreen describes the display our app is running on
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.makeKeyAndVisible
 
-    puts "Hello again!"
+    @blue_view = UIView.alloc.initWithFrame(CGRectMake(10,10,200,200))
+    @blue_view.backgroundColor = UIColor.blueColor
+    @window.addSubview(@blue_view)
 
+    @green_view = UIView.alloc.initWithFrame(CGRectMake(30,30,40,40))
+    @green_view.backgroundColor = UIColor.greenColor
+    @window.addSubview(@green_view)
+
+    @red_view = UIView.alloc.initWithFrame(CGRectMake(30,30,40,40))
+    @red_view.backgroundColor = UIColor.redColor
+    @blue_view.addSubview(@red_view)
     true
   end
 end
