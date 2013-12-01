fdea0664f0d75980976020a8627e26c2fcf63af7
taea <igiari@gmail.com>
2013-12-02 04:13:53 +0900

    Controllers

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -4,17 +4,8 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.makeKeyAndVisible
 
-    @blue_view = UIView.alloc.initWithFrame(CGRectMake(10,10,200,200))
-    @blue_view.backgroundColor = UIColor.blueColor
-    @window.addSubview(@blue_view)
+    @window.rootViewController = TapController.alloc.initWithNibName(nil, bundle: nil)
 
-    @green_view = UIView.alloc.initWithFrame(CGRectMake(30,30,40,40))
-    @green_view.backgroundColor = UIColor.greenColor
-    @window.addSubview(@green_view)
-
-    @red_view = UIView.alloc.initWithFrame(CGRectMake(30,30,40,40))
-    @red_view.backgroundColor = UIColor.redColor
-    @blue_view.addSubview(@red_view)
     true
   end
 end
