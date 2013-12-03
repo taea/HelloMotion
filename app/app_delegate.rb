0f688d23283df450e2d134c98e1335da6c872d10
taea <igiari@gmail.com>
2013-12-04 01:51:18 +0900

    Animation

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,25 +1,31 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    # UIScreen describes the display our app is running on
-    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
     @window.makeKeyAndVisible
+    
+    @points = [[0, 0], [50, 0], [0, 50], [50, 50]]
+    @current_index = 0
 
-    controller = TapController.alloc.initWithNibName(nil, bundle: nil)
-    nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
+    @view = UIView.alloc.initWithFrame [@points[@current_index], [100, 100]]
+    @view.backgroundColor = UIColor.blueColor
+    @window.addSubview(@view)
 
-    other_controller = UIViewController.alloc.initWithNibName(nil, bundle: nil)
-    other_controller.title = "Other"
-    other_controller.view.backgroundColor = UIColor.purpleColor
-    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
-    tab_controller.viewControllers = [nav_controller, other_controller]
-    @window.rootViewController = tab_controller
+    animate_to_next_point
 
-    controller = TapController.alloc.initWithNibName(nil, bundle: nil)
-    nav_controller = UINavigationController.alloc.initWithRootViewController(controller)
-    alphabet_controller = AlphabetController.alloc.initWithNibName(nil, bundle: nil)
-    tab_controller = UITabBarController.alloc.initWithNibName(nil, bundle: nil)
-    tab_controller.viewControllers = [alphabet_controller, nav_controller]
-    @window.rootViewController = tab_controller
     true
+  end
+
+  def animate_to_next_point
+    @current_index += 1
+    @current_index = @current_index % @points.count
+
+    UIView.animateWithDuration(0.3,
+      animations: lambda {
+        @view.frame = [@points[@current_index], [100, 100]]
+      },
+      completion:lambda {|finished|
+        self.animate_to_next_point
+      }
+    )
   end
 end
